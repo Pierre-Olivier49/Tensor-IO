21d8b8ec98f807dbc4bbbabe25267901a7e4502b
Pierre-Olivier49 <168573252+Pierre-Olivier49@users.noreply.github.com>
2024-05-18 21:21:53 -0400

    Update BaseTensor.h

---
+++ BaseTensor.h
@@ -0,0 +1,26 @@
+/*
+ * BaseTensor.h
+ *
+ *  Created on: Mar. 18, 2024
+ *       Author: Justin Pasztor ID: 40281784, Pierre-Olivier Marquis ID:40284322
+ */
+
+#ifndef BASETENSOR_H_
+#define BASETENSOR_H_
+#include <random>
+using namespace std;
+
+class BaseTensor {
+public:
+	virtual void loadData() = 0;
+	double valueGen(){ //Value Generation function
+		std::random_device rd;
+		std::mt19937 mt(rd());
+		std::uniform_real_distribution<double> dist(0.0, 1.0);
+		double randomValue = dist(mt);
+		return randomValue;
+	}
+
+};
+
+#endif /* BASETENSOR_H_ */
